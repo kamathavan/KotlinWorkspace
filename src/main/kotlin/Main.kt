1253fa51d2345304d07b61e435612a8ea3d186da
Mathavan Kaliyaperumal <mathucs@gmail.com>
2023-09-15 16:20:23 +0530

    smart casting input value

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,40 @@
-fun checkType(input: Any?) {
+fun getMessage(input: Int) = if (input > 3) {
+    "Given is bigger than 3"
+} else {
+    "Given is value is not more 3"
+}
+
+fun checkWhenMessage(input: Int) = when {
+    input > 3 -> "Given is bigger than 3"
+    else -> "Given is value is not more 3"
+}
+
+fun checkWhenMessage1(input: Int):String {
+    val message = when (input) {
+        3 -> "Given input value is 3"
+        else -> "Given input value is not 3"
+    }
+    return message
+}
+
+fun checkWhenMessage2(input: Int) {
     when (input) {
-        is String -> println("It is String value")
-        is Int -> println("It is Integer value")
-        null -> println("It is null value---->")
-        else -> println("It is unknown input value")
+        in 1..10 -> print("value range from 1 to 10")
+        35 -> print("It is valid value for 35")
+        !in 10..20 -> print("x is outside the range")
+        else -> print("none of the above")
     }
 }
 
 fun main() {
-    checkType(7.3)
+
+    println(getMessage(2))
+
+    println(checkWhenMessage(3))
+
+    println(checkWhenMessage1(4))
+
+    checkWhenMessage2(101)
 }
 
 
