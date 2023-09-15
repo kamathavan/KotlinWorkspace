a67d2ef821941a5d45043003893b185d5aa0c43b
Mathavan Kaliyaperumal <mathucs@gmail.com>
2023-09-15 16:47:27 +0530

    smart casting input value

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,7 @@ fun checkWhenMessage(input: Int) = when {
     else -> "Given is value is not more 3"
 }
 
-fun checkWhenMessage1(input: Int):String {
+fun checkWhenMessage1(input: Int): String {
     val message = when (input) {
         3 -> "Given input value is 3"
         else -> "Given input value is not 3"
@@ -19,12 +19,27 @@ fun checkWhenMessage1(input: Int):String {
 
 fun checkWhenMessage2(input: Int) {
     when (input) {
-        in 1..10 -> print("value range from 1 to 10")
-        35 -> print("It is valid value for 35")
-        !in 10..20 -> print("x is outside the range")
-        else -> print("none of the above")
+        in 1..10 -> println("value range from 1 to 10")
+        35 -> println("It is valid value for 35")
+        !in 10..20 -> println("x is outside the range")
+        else -> println("none of the above")
     }
 }
+
+fun checkFDataType(input: Any?) {
+    when (input) {
+        is String -> println("This is string type value")
+        is Number -> if (input is Int) {
+            println("This is integer type")
+        } else {
+            println("This is not number type s")
+        }
+
+        null -> println("This is null value")
+        else -> println("Unknow type of the value")
+    }
+}
+
 
 fun main() {
 
@@ -35,6 +50,14 @@ fun main() {
     println(checkWhenMessage1(4))
 
     checkWhenMessage2(101)
+
+    checkFDataType(null)
+
+    checkFDataType(4)
+
+    checkFDataType(4.5)
+
+    checkFDataType("Mathavan")
 }
 
 
